406c8bb9406bb6b56a99143b1eab2dc66772f56d
ironmaniiith <aalekhj2507@gmail.com>
2016-09-08 23:45:18 +0530

    added `play` function and its dependencies in `keyboard.js`

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,5 +1,6 @@
 document.onkeydown = function (n) {
     var t = window.event ? n.keyCode : n.which;
+    console.log(t);
     n.shiftKey ? notes.b_c.indexOf(t) != -1 && piano.music("b" + t, 3) : notes.w_c.indexOf(t) != -1 && piano.music("a" + t, 1)
 };
 document.onmouseup = function () {
@@ -8,3 +9,34 @@ document.onmouseup = function () {
 document.getElementById("piano").onmousedown = function () {
     piano.isMouseDown = !0
 };
+
+var lookup = {};
+for (var i = 0; i < 300; i++) {
+    lookup[String.fromCharCode(i)] = i;
+}
+
+function getKeyCode(c) {
+    return lookup[c];
+}
+
+function isShiftKeyEnabled(c) {
+    return notes.b_n.indexOf(c) !== -1;
+}
+
+function play(c) {
+    var n = {};
+    n.shiftKey = false;
+    var pos = notes.b_n.indexOf(c);
+    if (pos !== -1) {
+        var val = notes.b_n_[pos];
+        n.keyCode = getKeyCode(val.toUpperCase());
+    } else {
+        n.keyCode = getKeyCode(c.toUpperCase());
+    }
+    console.log(n);
+    if (isShiftKeyEnabled(c)) {
+        n.shiftKey = true;
+    }
+    var t = n.keyCode;
+    n.shiftKey ? notes.b_c.indexOf(t) != -1 && piano.music("b" + t, 3) : notes.w_c.indexOf(t) != -1 && piano.music("a" + t, 1)
+}
